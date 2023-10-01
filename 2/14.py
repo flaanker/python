1bc4a0cd2480a5a88566544b2ad7fb89b4143237
flaanker <144709503+flaanker@users.noreply.github.com>
2023-10-01 19:00:31 +0300

    Create 14.py

---
+++ 2/14.py
@@ -0,0 +1,12 @@
+import turtle as f1
+f1.shape('turtle')
+f1.speed(10)
+for i in range (5):
+    f1.right(180-180/5)
+    f1.forward(100)
+f1.penup()
+f1.goto(300,300)
+f1.pendown()
+for i in range (11):
+    f1.right(180-180/11)
+    f1.forward(100)
